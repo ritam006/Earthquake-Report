ef755781eb5aab323f638eb2ed1585da63b9846a
ritam006 <ritammallick.mallick9@gmail.com>
2017-01-05 22:52:24 +0530

    Attempt to access the JSON string through URL

--- app/src/main/java/com/android/ritamthegreat/earthquake/MainActivity.java
+++ app/src/main/java/com/android/ritamthegreat/earthquake/MainActivity.java
@@ -2,11 +2,13 @@ package com.android.ritamthegreat.earthquake;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,34 +16,32 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private static final String USGS_REQUEST_URL ="http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+    private final String LOG_TAG=MainActivity.class.getSimpleName();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ArrayList<CustomString> earthquakes=QueryUtils.extractEarthquakes();
-
-        ListView listView=(ListView)findViewById(R.id.list);
-        final WordAdapter adapter=new WordAdapter(this,earthquakes);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                CustomString string=adapter.getItem(i);
-                String url=string.getURL();
-                Intent intent=new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(url));
-                if(intent.resolveActivity(getPackageManager())!=null){
-                    startActivity(intent);
-                }
-            }
-        });
+        EarthquakeAsyncTask task=new EarthquakeAsyncTask();
+        task.execute();
 
     }
 
@@ -66,4 +66,125 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * {@link AsyncTask} to perform the network request on a background thread, and then
+     * update the UI with the first earthquake in the response.
+     */
+    private class EarthquakeAsyncTask extends AsyncTask<URL, Void, WordAdapter> {
+
+        @Override
+        protected WordAdapter doInBackground(URL... urls) {
+            // Create URL object
+            URL url = createUrl(USGS_REQUEST_URL);
+
+            // Perform HTTP request to the URL and receive a JSON response back
+            String jsonResponse = "";
+            try {
+                jsonResponse = makeHttpRequest(url);
+            } catch (IOException e) {
+                // TODO Handle the IOException
+            }
+
+            ArrayList<CustomString> earthquakes=QueryUtils.extractEarthquakes();
+
+            final WordAdapter adapter=new WordAdapter(MainActivity.this,earthquakes);
+
+            return adapter;
+        }
+
+        /**
+         * Update the screen with the given earthquake (which was the result of the
+         * {@link EarthquakeAsyncTask}).
+         */
+        @Override
+        protected void onPostExecute(WordAdapter adapter) {
+
+            if (adapter == null) {
+                return;
+            }
+            ListView listView=(ListView)findViewById(R.id.list);
+
+            listView.setAdapter(adapter);
+
+            final WordAdapter newAdapter=adapter;
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    CustomString string=newAdapter.getItem(i);
+                    String url=string.getURL();
+                    Intent intent=new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    if(intent.resolveActivity(getPackageManager())!=null){
+                        startActivity(intent);
+                    }
+                }
+            });
+
+        }
+
+        /**
+         * Returns new URL object from the given string URL.
+         */
+        private URL createUrl(String stringUrl) {
+            URL url = null;
+            try {
+                url = new URL(stringUrl);
+            } catch (MalformedURLException exception) {
+                Log.e(LOG_TAG, "Error with creating URL", exception);
+                return null;
+            }
+            return url;
+        }
+
+        /**
+         * Make an HTTP request to the given URL and return a String as the response.
+         */
+        private String makeHttpRequest(URL url) throws IOException {
+            String jsonResponse = "";
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                urlConnection.connect();
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } catch (IOException e) {
+                // TODO: Handle the exception
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (inputStream != null) {
+                    // function must handle java.io.IOException here
+                    inputStream.close();
+                }
+            }
+            return jsonResponse;
+        }
+
+        /**
+         * Convert the {@link InputStream} into a String which contains the
+         * whole JSON response from the server.
+         */
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return output.toString();
+        }
+    }
+
+
 }
